dbeb90b9b85db9c84b439d6e85bd2b25fd42bb5e
abdalllah22 <ahmed.abdalllah22@gmail.com>
2021-08-26 04:34:50 +0200

    first commit

---
+++ db/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log('Database Connection is ready... ')
+})
+.catch((err)=>{
+    console.log(err);
+    console.log("---> Database Connection is not ready <---")
+})
+
+
+
+
+
+
